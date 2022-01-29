56bb2582838c23081ef01921335c12568b7a9dd4
Jaeyeon Park <donionb@yonsei.ac.kr>
2022-01-29 11:11:42 +0900

    feat : add Leetcode String problem solution

---
+++ Leetcode/src/string/palinstring/Solution.java
@@ -0,0 +1,35 @@
+package string.palinstring;
+
+
+/*
+   리트코드 5번 문제
+   문자열 처리 문제이다. 수행시간이 오래 걸린 이유 : 문자열 관련 연산은 수행 시간이 오래 걸린다
+   따라서 문자열 처리 연산은 최대한 적게 해야할듯
+ */
+
+
+class Solution {
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) return "";
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    private int expandAroundCenter(String s, int left, int right) {
+        int L = left, R = right;
+        while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
+            L--;
+            R++;
+        }
+        return R - L - 1;
+    }
+}
